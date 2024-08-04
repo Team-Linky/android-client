aa3970e62338bc8dcebc715f123f7732fdce3a4c
sun5066 <alstjr4434@naver.com>
2024-08-04 23:43:16 +0900

    Feature: Version Update
    1.0.05 -> 1.0.06

--- build-logic/convention/src/main/kotlin/com/linky/convention/VersionConstants.kt
+++ build-logic/convention/src/main/kotlin/com/linky/convention/VersionConstants.kt
@@ -5,7 +5,7 @@ object VersionConstants {
     const val MIN_SDK = 24
     const val TARGET_SDK = 34
 
-    const val VERSION_CODE = 5
-    const val VERSION_NAME = "1.0.05"
+    const val VERSION_CODE = 6
+    const val VERSION_NAME = "1.0.06"
 
 }
